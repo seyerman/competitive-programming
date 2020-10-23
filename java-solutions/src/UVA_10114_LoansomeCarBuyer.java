35347e1bf5231c4ac643e00602a8490718c70db6
Juan Manuel Reyes Garcia <seyerman@gmail.com>
2020-10-23 00:02:38 -0500

    UVA_10114_LoansomeCarBuyer Accepted

--- java-solutions/src/UVA_10114_LoansomeCarBuyer.java
+++ java-solutions/src/UVA_10114_LoansomeCarBuyer.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class UVA_10114_LoansomeCarBuyer {
 	public static BufferedReader br;
