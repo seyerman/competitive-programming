fed3d853e3fe265ac2ea312e79a743bd0a59c0c1
Juan Manuel Reyes Garcia <seyerman@gmail.com>
2020-12-01 21:30:33 -0500

    Interview Preparation Kit - WarmUp Challenges - Sorting Bubble Sort Solved

---
+++ java-solutions/src/InterviewPreparationKit_WarmUpChallenges_SortingBubbleSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class InterviewPreparationKit_WarmUpChallenges_SortingBubbleSort {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        boolean sorted = false;
+        int swaps = 0;
+        while(!sorted){
+            sorted = true;
+            for(int i=0;i<a.length-1;i++){
+                if(a[i]>a[i+1]){
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                    swaps++;
+                    sorted = false;
+                }
+            }
+        }
+        System.out.println("Array is sorted in "+swaps+" swaps.");
+        System.out.println("First Element: "+a[0]);
+        System.out.println("Last Element: "+a[a.length-1]);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
